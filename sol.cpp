0b95fc508376a42fab9d06b51b05ff6902f9789d
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-29 17:45:32 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,28 +1,32 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-#define ll long long
+using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
 	int q;
 	cin >> q;
-	map<string, int> mp;
-	for(int i=0; i<q; ++i) {
-		int t;
+	map<string, int> m;
+	// simply follow the instructions in the problem statement
+	for (int i = 0; i < q; i++) {
+		int type;
 		string x;
-		cin >> t >> x;
-		if (t==1) {
+		cin >> type >> x;
+		if (type == 1) {
 			int y;
 			cin >> y;
-			mp[x]+=y;
+			map<string, int>::iterator itr = m.find(x);
+			if (itr == m.end()) {
+				m.insert(make_pair(x, y));
+			} else {
+				m[x] = m[x] + y;
+			}
+		} else if (type == 2) {
+			m.erase(x);
+		} else {
+			cout << m[x] << '\n';
 		}
-		else if(t==2) {
-			mp.erase(x);
-		}
-		else
-			cout << mp[x] << "\n";
 	}
+	return 0;
 }
